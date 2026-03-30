e613b8e56fc926008a2abc8e3a4a982a43ccbe6b
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2026-03-30 10:00:50 +0200

    Read exclusions from system

--- src/main/scala/asimovsn/rules.scala
+++ src/main/scala/asimovsn/rules.scala
@@ -19,47 +19,50 @@ enum Rule:
       Option.when(ab.endsWith(fp) && fp.endsWith(ab))(ab)
 
 object Rules:
-  private[asimovsn] def ws(using P[?]) = P(
-    CharsWhile(c => c == ' ' || c == '\t', min = 0)
-  )
-  private[asimovsn] def comment(using P[?]) = P(
-    "#" ~ CharsWhile(c => c != '\n' && c != '\r', min = 0)
-  )
-  private[asimovsn] def content(using P[?]) = P(
-    CharsWhile(c => !c.isWhitespace && c != '#').!
-  ).map(_.trim)
+  private[asimovsn] object parser {
+    type Ctx = P[?]
+    def ws(using Ctx) = P(
+      CharsWhile(c => c == ' ' || c == '\t', min = 0)
+    )
+    def comment(using Ctx) = P(
+      "#" ~ CharsWhile(c => c != '\n' && c != '\r', min = 0)
+    )
+    def content(using Ctx) = P(
+      CharsWhile(c => !c.isWhitespace && c != '#').!
+    ).map(_.trim)
 
-  private[asimovsn] def lineEnd(using P[?]) = P(
-    ws ~ comment.? ~ ("\n" | "\r\n")
-  )
-  private[asimovsn] def sentinelDirective(using P[?]) =
-    P("SENT" ~ ws ~ content ~ ws ~ content ~ ws)
-      .map(Rule.SentilDir.apply)
+    def lineEnd(using Ctx) = P(
+      ws ~ comment.? ~ ("\n" | "\r\n")
+    )
+    def sentinelDirective(using Ctx) =
+      P("SENT" ~ ws ~ content ~ ws ~ content ~ ws)
+        .map(Rule.SentilDir.apply)
 
-  private[asimovsn] def dirNameDirective(using P[?]) =
-    P("SIMP" ~ ws ~ content).map(Rule.DirOnly.apply)
+    def dirNameDirective(using Ctx) =
+      P("SIMP" ~ ws ~ content).map(Rule.DirOnly.apply)
 
-  private[asimovsn] def fullpathDirective(using P[?]) =
-    P("FULL" ~ ws ~ content ~ ws).map { (p: String) =>
-      val parsed = os.Path(p)
-      // assert(os.isDir(parsed))
-      Rule.FullPath(parsed.toNIO)
-    }
+    def fullpathDirective(using Ctx) =
+      P("FULL" ~ ws ~ content ~ ws).map { (p: String) =>
+        val parsed = os.Path(p)
+        // assert(os.isDir(parsed))
+        Rule.FullPath(parsed.toNIO)
+      }
 
-  private[asimovsn] def blankOrComment(using P[?]) =
-    P(ws ~ comment.? ~ lineEnd).map(_ => None)
+    def blankOrComment(using Ctx) =
+      P(ws ~ comment.? ~ lineEnd).map(_ => None)
 
-  private[asimovsn] def validCommand(using P[?]) =
-    P(
-      ws ~ (sentinelDirective | dirNameDirective | fullpathDirective) ~ comment.? ~ lineEnd
-    ).map(Some(_))
+    def validCommand(using Ctx) =
+      P(
+        ws ~ (sentinelDirective | dirNameDirective | fullpathDirective) ~ comment.? ~ lineEnd
+      ).map(Some(_))
 
-  private[asimovsn] def anyLine(using P[?]) = P(validCommand | blankOrComment)
+    def anyLine(using Ctx) = P(validCommand | blankOrComment)
 
-  private[asimovsn] def file(using P[?]) = P(anyLine.rep ~ End).map(_.flatten)
+    def file(using Ctx) = P(anyLine.rep ~ End).map(_.flatten)
+  }
 
   def process(input: String) =
-    parse(input, p => file(using p)) match
+    parse(input, p => parser.file(using p)) match
       case Parsed.Success(records, _) => records.toList
       case f: Parsed.Failure          =>
         throw new Exception(s"Parse Error: ${f.trace().aggregateMsg}")
