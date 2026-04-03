5e76763c53d7124b70b0c056642dbbbe4865e84e
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2026-04-03 21:16:49 +0200

    Improve

--- src/main/scala/asimovsn/config.scala
+++ src/main/scala/asimovsn/config.scala
@@ -10,16 +10,19 @@ case class Config(
 )
 
 object Config:
-    def parse(args: Array[String]) =
-        val builder = OParser.builder[Config]
-        val parser = {
-            import builder._
-            OParser.sequence(
-                programName("asimov-sn"),
-                opt[Boolean]('d', "dry-run").text("print exclusions without applying"),
-                opt[Path]('r', "--rules").text("List of rules").action((x, c) => c.copy(rules=Rules.parse(os.read(os.Path(x))))),
-                opt[Path]('b', "base-path").text("Base path to search").action((x, c) => c.copy(basePath = os.Path(x)))
-            )
-        }
-        OParser.parse(parser, args, Config())
-
+  def parse(args: Array[String]) =
+    val builder = OParser.builder[Config]
+    val parser = {
+      import builder._
+      OParser.sequence(
+        programName("asimov-sn"),
+        opt[Boolean]('d', "dry-run").text("print exclusions without applying"),
+        opt[Path]('r', "--rules")
+          .text("List of rules")
+          .action((x, c) => c.copy(rules = Rules.parse(os.read(os.Path(x))))),
+        opt[Path]('b', "base-path")
+          .text("Base path to search")
+          .action((x, c) => c.copy(basePath = os.Path(x)))
+      )
+    }
+    OParser.parse(parser, args, Config())
